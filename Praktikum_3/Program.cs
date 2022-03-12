b9d25fd8f9c9e2b4144c98bad8b92e598468071b
luthfi55 <luthfi12354@gmail.com>
2022-03-12 15:35:45 +0700

    first commit

---
+++ Praktikum_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Praktikum_3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
